7b8123c63de83a55a5f1d496d22367bb557ca0c8
leeyj3382 <leeyj3382@naver.com>
2026-02-11 23:09:05 +0900

    2026-02-11

--- sql/흉부외과 또는 일반외과 의사 목록 출력하기.sql
+++ sql/흉부외과 또는 일반외과 의사 목록 출력하기.sql
@@ -1,5 +1,5 @@
-select USER_ID, PRODUCT_ID
-from ONLINE_SALE
-group by USER_ID, PRODUCT_ID
-having count(*) >= 2
-order by USER_ID asc, PRODUCT_ID desc
+select DR_NAME, DR_ID, MCDP_CD, DATE_FORMAT(HIRE_YMD, "%Y-%m-%d") as HIRE_YMD
+from DOCTOR
+where MCDP_CD = 'CS'
+or MCDP_CD = 'GS'
+order by HIRE_YMD desc, DR_NAME asc
