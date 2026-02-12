2919c9d3ea108fcf1f3e83e22fc49927b47396ce
leeyj3382 <leeyj3382@naver.com>
2026-02-12 22:27:26 +0900

    2026-02-11

---
+++ sql/3월에 태어난 여성 회원 목록 출력하기.sql
@@ -0,0 +1,6 @@
+select MEMBER_ID, MEMBER_NAME, GENDER, DATE_FORMAT(DATE_OF_BIRTH, "%Y-%m-%d") as DATE_OF_BIRTH
+from member_profile
+where gender = 'w'
+and month(date_of_birth) = 3
+and tlno is not null
+order by member_id asc
